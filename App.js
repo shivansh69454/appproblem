2fdcc50a1464010d46b603f964796af0d56425ef
shivansh <shivansh@shivanshs-Laptop.local>
2026-04-25 15:37:11 +0530

    final working

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import 'react-native-url-polyfill/auto';
+
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
